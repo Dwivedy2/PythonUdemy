5e5448a43a065c7cdc4c2e7877e96b9921717f22
Dwivedy2 <121598894+Dwivedy2@users.noreply.github.com>
2025-11-24 12:28:33 +0530

    Update chapter_2.py

--- python-udemy-main/02_datatypes/chapter_2.py
+++ python-udemy-main/02_datatypes/chapter_2.py
@@ -4,5 +4,7 @@ print(f"Initial spice mix id: {spice_mix}")
 spice_mix.add("Ginger")
 spice_mix.add("cardamom")
 spice_mix.add("lemon")
+# here this set datatype is mutable, which means it can be changed first be addng Ginger,
+# cardamom, lemon and the id is not changed so this datatype is mutable
 print(f"Initial spice mix id: {spice_mix}")
 print(f"After spice mix id: {id(spice_mix)}")
