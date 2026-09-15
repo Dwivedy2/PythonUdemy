8c267e3294bddd3be465df103e93e8c829da3d7b
Dwivedy2 <121598894+Dwivedy2@users.noreply.github.com>
2026-09-15 14:47:51 +0000

    datatypes notes

--- python-udemy-main/02_datatypes/chapter_1.py
+++ python-udemy-main/02_datatypes/chapter_1.py
@@ -6,3 +6,8 @@ print(f"Second Initial sugar: {sugar_amount}")
 
 print(f"ID of 2: {id(2)}")
 print(f"ID of 12: {id(12)}")
+
+# my takeaway
+# sugar_amount - mutable
+# 2, 12 -> immutable
+# only refrence is changed
